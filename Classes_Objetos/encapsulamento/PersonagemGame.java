ce57461e69039b11ba44d602d75e63147e3bd17b
guilhermealvino <guilhermealvi9@gmail.com>
2025-08-04 08:31:06 -0300

    Teste Encapsulamento

--- Classes_Objetos/encapsulamento/PersonagemGame.java
+++ Classes_Objetos/encapsulamento/PersonagemGame.java
@@ -1,5 +1,3 @@
-package Classes_Objetos.encapsulamento;
-
 public class PersonagemGame {
     private int saudeAtual;
     private String nome;
@@ -18,19 +16,5 @@ public class PersonagemGame {
 
     public void setNome(String nome) {
         this.nome = nome;
-    }
-
-    public void tomarDano(int quantidadeDeDano) {
-        saudeAtual -= quantidadeDeDano;
-        if (saudeAtual < 0) {
-            saudeAtual = 0;
-        }
-    }
-
-    public void receberCura(int quantidadeDeCura) {
-        saudeAtual += quantidadeDeCura;
-        if (saudeAtual > 100) {
-            saudeAtual = 100;
-        }
     }
 }
