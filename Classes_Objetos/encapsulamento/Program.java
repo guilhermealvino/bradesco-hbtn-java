e2173f98b33fcc16c20f2e87a96595d819ec34de
guilhermealvino <guilhermealvi9@gmail.com>
2025-08-04 08:32:24 -0300

    Teste Encapsulamento

--- Classes_Objetos/encapsulamento/Program.java
+++ Classes_Objetos/encapsulamento/Program.java
@@ -5,18 +5,6 @@ public class Program {
         personagem1.setNome("Sonic");
         personagem1.setSaudeAtual(10);
 
-        System.out.printf("%d\n", personagem1.getSaudeAtual());
-
-        personagem1.receberCura(91);
-
-        System.out.printf("%d\n", personagem1.getSaudeAtual());
-
-        personagem1.tomarDano(25);
-
-        System.out.printf("%d\n", personagem1.getSaudeAtual());
-
-        personagem1.tomarDano(80);
-
-        System.out.printf("%d\n", personagem1.getSaudeAtual());
+        System.out.printf("Nome: %s - Saude: %d\n", personagem1.getNome(), personagem1.getSaudeAtual());
     }
 }
